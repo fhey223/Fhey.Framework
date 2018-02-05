1b5e92fe6bf17d5496c8dd0788d1a5b9cb2b2cd0
jie <jie@fhey.me>
2018-02-05 16:19:35 +0800

    框架改为4.7.1

--- Fhey.Framework.Autofac/Class1.cs
+++ Fhey.Framework.Autofac/Class1.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fhey.Framework.Validate
+namespace Fhey.Framework.Autofac
 {
-    //
     public class Class1
     {
     }
