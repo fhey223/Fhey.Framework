b6753fd2a83fef4b986554341a72d7cc5f854464
jie <jie@fhey.me>
2018-02-13 17:47:59 +0800

    更名

--- Fhey.framework.MyBatis/Properties/AssemblyInfo.cs
+++ Fhey.framework.MyBatis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Fhey.framework.MyBatis")]
+[assembly: AssemblyTitle("Fhey.Framework.MyBatis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fhey.framework.MyBatis")]
+[assembly: AssemblyProduct("Fhey.Framework.MyBatis")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2fbd9252-393b-48dc-ac9e-551d5ba87b97")]
+[assembly: Guid("503b82ca-4f05-4cc3-af61-f02e85d18205")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
