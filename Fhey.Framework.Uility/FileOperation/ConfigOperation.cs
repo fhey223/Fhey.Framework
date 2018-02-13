30b2452afb3f16fcd3913f42340a6e4d26af739a
董炜杰 <dongwj5@centaline.com.cn>
2018-02-13 13:13:51 +0800

    配置文件操作

---
+++ Fhey.Framework.Uility/FileOperation/ConfigOperation.cs
@@ -0,0 +1,61 @@
+﻿using System.Configuration;
+
+namespace Fhey.Framework.Uility.FileOperation
+{
+    /// <summary>
+    /// 经试验没有物理上改变config文件，但是在程序运行期间操作值是用变化的
+    /// </summary>
+    public class ConfigOperation
+    {
+        Configuration _obj;
+        KeyValueConfigurationCollection _config; 
+        public ConfigOperation()
+        {
+            _obj = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            _config = _obj.AppSettings.Settings;
+        }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        public string Get(string Key)
+        {
+            return _config[Key].Value;
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="Value"></param>
+        public void Add(string Key,string Value)
+        {
+            _config.Add (Key,Value);
+            _obj.Save();
+        }
+
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public void Set(string Key, string Value)
+        {
+             _config[Key].Value = Value;
+            _obj.Save();
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="Key"></param>
+        public void Remove(string Key)
+        {
+            _config.Remove(Key);
+            _obj.Save();
+        }
+    }
+}
--- UilityTest/Program.cs
+++ UilityTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,28 +13,26 @@ namespace UilityTest
     {
         static void Main(string[] args)
         {
-            var b = new int[]{1,2};
-            var a = JsonConvert.SerializeObject("er");
-            var a1 = JsonConvert.DeserializeObject<string>(a);
-
-            Set("1", a);
-
-
+            //获取Configuration对象
+            var config = new Fhey.Framework.Uility.FileOperation.ConfigOperation();
+            //根据Key读取<add>元素的Value
+            string name = config.Get("name");
+            //写入<add>元素的Value
+            config.Set("name", "fx163");
+            //增加<add>元素
+            config.Add("url", "http://www.fx163.net");
+            //删除<add>元素
+            config.Remove("name");
+            //一定要记得保存，写不带参数的config.Save()也可以
+            config.Save(ConfigurationSaveMode.Modified);
+            //刷新，否则程序读取的还是之前的值（可能已装入内存）
+            System.Configuration.ConfigurationManager.RefreshSection("appSettings");
         }
 
-        public static bool Set<T>(string key, T value, DateTime? expiresTime = default(DateTime?))
-        {
-
-            var a= expiresTime - DateTime.Now;
-            return true;
-        }
-
-        public static bool Set(string key, string value, DateTime? expiresTime = default(DateTime?))
-        {
-
-            var a = expiresTime - DateTime.Now;
-            return true;
-        }
-
+        
+    }
+    public class ConfigOperation
+    {
+        
     }
 }
