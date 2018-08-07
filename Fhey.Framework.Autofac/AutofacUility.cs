cf2ed263e67c455b976f37beaa4fcd61188f033f
jie <jie@fhey.me>
2018-08-07 23:18:16 +0800

    autofac自动注入添加 以特定字符结尾模式

--- Fhey.Framework.Autofac/AutofacUility.cs
+++ Fhey.Framework.Autofac/AutofacUility.cs
@@ -4,34 +4,57 @@ using AutoFacUntil.Attributes;
 using AutoFacUntil.Enum;
 using AutoFacUntil.Interface;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fhey.Framework.Autofac
 {
     public class AutofacUility
     {
+        /// <summary>Assembly
+        /// 调用者Assembly
+        /// </summary>
+        public Assembly CallingAssembly { get; set; }
+
         public AutofacUility()
         {
             CallingAssembly = Assembly.GetCallingAssembly();
         }
 
-        public Assembly CallingAssembly { get; set; }
-
+        /// <summary>
+        /// 自动装配(特性模式)
+        /// </summary>
+        /// <returns></returns>
         public AutofacDependencyResolver AutoRegisterByAttribute()
         {
             return AutoRegister(RegisterType.Attribute);
         }
 
+        /// <summary>
+        /// 自动装配(接口模式)
+        /// </summary>
+        /// <returns></returns>
         public AutofacDependencyResolver AutoRegisterByInterface()
         {
             return AutoRegister(RegisterType.Interface);
         }
 
-        public AutofacDependencyResolver AutoRegister(RegisterType type)
+        /// <summary>
+        /// 自动装配(特定字符串结尾模式)
+        /// </summary>
+        /// <returns></returns>
+        public AutofacDependencyResolver AutoRegisterByEndsWith(string endsWith)
+        {
+            return AutoRegister(RegisterType.EndsWith, endsWith);
+        }
+
+        /// <summary>
+        /// 自动装配
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="endsWith"></param>
+        /// <returns></returns>
+        public AutofacDependencyResolver AutoRegister(RegisterType type,string endsWith = null)
         {
             var builder = new ContainerBuilder();
             var types = builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies());
@@ -44,10 +67,17 @@ namespace Fhey.Framework.Autofac
                     var baseType = typeof(IAutoWrite);
                     types = types.Where(t => baseType.IsAssignableFrom(t) && t != baseType);
                     break;
+                case RegisterType.EndsWith:
+                    if (string.IsNullOrEmpty(endsWith))
+                    {
+                        return null;
+                    }
+                    types = types.Where(t => t.Name.EndsWith(endsWith));
+                    break;
             }
 
             types.AsImplementedInterfaces()
-                .SingleInstance();
+                .InstancePerDependency();
             builder.RegisterControllers(CallingAssembly).PropertiesAutowired();
             return new AutofacDependencyResolver(builder.Build());
         }
--- Fhey.Framework.Autofac/Enum/RegisterType.cs
+++ Fhey.Framework.Autofac/Enum/RegisterType.cs
@@ -2,7 +2,19 @@
 {
     public enum RegisterType
     {
+        /// <summary>
+        /// 特性模式
+        /// </summary>
         Attribute,
-        Interface
+
+        /// <summary>
+        /// 接口模式
+        /// </summary>
+        Interface,
+
+        /// <summary>
+        /// 以特定字符结尾模式
+        /// </summary>
+        EndsWith
     }
 }
